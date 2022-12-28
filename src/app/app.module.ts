a6f8cdb5497ae37a5162c0f7112fc33e09755fe9
William Dias Marcos <william.diasmarcos@gmail.com>
2022-12-28 10:58:41 -0300

    css editing and component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeScreenComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
