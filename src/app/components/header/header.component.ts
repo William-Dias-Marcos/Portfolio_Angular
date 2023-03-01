075acdb1754aebc6b1361a514ec764d8b488a37f
William Dias Marcos <william.diasmarcos@gmail.com>
2023-02-28 22:52:41 -0300

    sidebar creation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  @ViewChild('navigation_header') navigationHeader: any;
+  showSidebar: boolean = false;
 
+  togleSidebar() {
+    this.showSidebar = !this.showSidebar;
+    let sidebar = this.navigationHeader.nativeElement;
+
+    if (this.showSidebar) {
+      sidebar.style.top = '0%';
+    } else {
+      sidebar.style.top = '-100%';
+    }
+    // this.navigationHeader.nativeElement.classList.add('showSidebar');
+  }
 }
